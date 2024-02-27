01460e5b102aa94ee6af7c542bf405bed558ee14
Varun Kumar Prabhakar <vkprbhkr663@gmail.com>
2024-02-27 23:58:33 +0530

    Sentiment_analysis using NLP

---
+++ sentiment_analysis3.py
@@ -0,0 +1,53 @@
+# Text analysis using Natural Language processing NLTK libraries
+
+#Text analysis using local text file
+import nltk
+import string
+from collections import Counter
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords #imported inbuilt stopwords from NLTK library
+from scraper import sapiens
+text=sapiens
+lowercase=text.lower()
+clean_text=lowercase.translate(str.maketrans('','',string.punctuation))
+tokenized_word=nltk.word_tokenize(clean_text,"english")
+
+
+final_word=[]
+emotn_lst=[]
+
+for word in tokenized_word:
+    if word not in stopwords.words("english"):
+        final_word.append(word)
+
+with open("emotion.txt","r") as file:
+    for line in file:
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+
+        if word in final_word:
+            emotn_lst.append(emotion)
+print(emotn_lst)
+w=Counter(emotn_lst)
+print(w)
+def sentiment_analyze(s_text):
+    score=SentimentIntensityAnalyzer().polarity_scores(s_text)
+    neg=score["neg"]
+    pos=score["pos"]
+    if neg>pos:
+        print("Negetive Sentiment")
+    elif pos>neg:
+        print("Positive Sentiment")
+    else:
+        print("Neutral sentiment")
+        
+sentiment_analyze(clean_text)
+
+#plot graph
+fig,ax1=plt.subplots()
+ax1.bar(w.keys(),w.values())
+fig.autofmt_xdate()
+plt.show()
+
+

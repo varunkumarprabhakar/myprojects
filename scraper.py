4ec0e35acc1868c281b15f6200604b034440e6ea
Varun Kumar Prabhakar <47877937+varunkumarprabhakar@users.noreply.github.com>
2024-03-04 11:29:53 +0530

    Delete scraper.py

--- scraper.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-req=requests.get("https://opennews.org/blog/opennews-hiring-co-director/")
-soup=BeautifulSoup(req.content,"html.parser")
-res=soup.title
-# sapiens=res.get_text()
-sapiens=soup.get_text()

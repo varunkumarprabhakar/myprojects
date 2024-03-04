341195dabb64ed9f1c90119d1c3f337631b90f65
Varun Kumar Prabhakar <47877937+varunkumarprabhakar@users.noreply.github.com>
2024-03-04 11:39:52 +0530

    Quizz Game

---
+++ KBC.py
@@ -0,0 +1,65 @@
+print("Welcome to Kaun Banega Crorepati")
+
+print("1. Garampani Sanctuary is in one of the following locations:")
+a=print("(a) Junagarh,Gujarat")
+b=print("(b) Kohima, Nagaland")
+c=print("(c) Diphu, Assam")
+d=print("(d) Gangtok, Sikkim")
+x=input("Enter your option = ")
+match(x):
+    case "a":
+                    print("wrong")
+    case "b":
+                    print("wrong")
+    case "c":
+                    print("Correct")
+    case "d":
+                    print("wrong")
+
+print("2. What is entomology study? :")
+a=print("(a) Human Beings\’ Behaviour ")
+b=print("(b) Insects ")
+c=print("(c) The history and origins of technical and scientific words ")
+d=print("(d) The process by which rocks form ")
+y=input("Enter your option = ")
+match(y):
+            case "a":
+                            print("you loose")
+            case "b":
+                            print("you win")
+            case "c":
+                            print("you loose")
+            case "d":
+                            print("you loose")
+print("3. Galileo was an astronomer who :")
+a=print("(a) developed the telescope ")
+b=print("(b) discovered four satellites of Jupiter")
+c=print("(c) The history and origins of technical and scientific words ")
+d=print("(d) found that the movement of the pendulum produces a regular time measurement")
+z=input("Enter your option = ")
+match(z):
+            case "a":
+                            print("you loose")
+            case "b":
+                            print("you win")
+            case "c":
+                            print("you loose")
+            case "d":
+                            print("you loose")
+print("4.Sunlight exposure can boost your health for which the following reasons  :")
+a=print("(a) the infrared light kills bacteria in the body ")
+b=print("(b) resistance power increases ")
+c=print("(c) the pigment cells in the skin get stimulated and produce a healthy tan ")
+d=print("(d)the ultraviolet rays convert skin oil into Vitamin D")
+w=input("Enter your option = ")
+match(w):
+            case "a":
+                            print("you loose")
+            case "b":
+                            print("you loose")
+            case "c":
+                            print("you loose")
+            case "d":
+                            print("you win")
+
+
